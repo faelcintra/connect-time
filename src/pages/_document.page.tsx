b6c8cab50ddee5bfac47237fb7068628e9fee3d2
faelcintra <rafaacintramartins@gmail.com>
2023-06-29 23:57:03 -0300

    feat: :building_construction: Document page

---
+++ src/pages/_document.page.tsx
@@ -0,0 +1,29 @@
+import { getCssText } from '@ignite-ui/react'
+import { Head, Html, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700;1,800&family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <script
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
