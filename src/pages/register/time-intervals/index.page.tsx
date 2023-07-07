4189ad53e6f815a69a1455d9666c0cb049955145
faelcintra <rafaacintramartins@gmail.com>
2023-07-07 09:51:52 -0300

    feat: :sparkles: Added refined and validated timeIntervalsFormSchema for data consistency

---
+++ src/pages/register/time-intervals/index.page.tsx
@@ -0,0 +1,161 @@
+import {
+  Button,
+  Checkbox,
+  Heading,
+  MultiStep,
+  Text,
+  TextInput,
+} from '@ignite-ui/react'
+import { Container, Header } from '../styles'
+import {
+  IntervalBox,
+  IntervalDay,
+  IntervalInputs,
+  IntervalItem,
+  IntervalsContainer,
+  FormError,
+} from './styles'
+import { ArrowRight } from 'phosphor-react'
+import { useFieldArray, useForm, Controller } from 'react-hook-form'
+import { getWeekDays } from '@/src/utils/get-week-days'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { convertTimeInMinutes } from '@/src/utils/convert-time-in-minutes'
+
+const timeIntervalsFormSchema = z.object({
+  intervals: z
+    .array(
+      z.object({
+        weekDay: z.number().min(0).max(6),
+        checked: z.boolean(),
+        startTime: z.string(),
+        endTime: z.string(),
+      }),
+    )
+    .length(7)
+    .transform((intervals) => intervals.filter((interval) => interval.checked))
+    .refine((intervals) => intervals.length > 0, {
+      message: 'Selecione no minimo um dia da semana!',
+    })
+    .transform((intervals) => {
+      return intervals.map((interval) => {
+        return {
+          weekDay: interval.weekDay,
+          startTimeMinutes: convertTimeInMinutes(interval.startTime),
+          endTimeMinutes: convertTimeInMinutes(interval.endTime),
+        }
+      })
+    })
+    .refine(
+      (intervals) => {
+        return intervals.every(
+          // se todos tem uma diferença de pelomenos uma hora
+          (interval) =>
+            interval.endTimeMinutes - 60 >= interval.startTimeMinutes,
+        )
+      },
+      {
+        message:
+          'O horário de término deve ter no minimo 1hr distante do início.',
+      },
+    ),
+})
+
+type TimeIntervalsFormInput = z.input<typeof timeIntervalsFormSchema> // os dados como entra no zod
+type TimeIntervalsFormOutput = z.output<typeof timeIntervalsFormSchema> // os dados como sai do zod
+
+export default function TimeIntervals() {
+  const {
+    formState: { isSubmitting, errors },
+    control,
+    handleSubmit,
+    register,
+    watch,
+  } = useForm<TimeIntervalsFormInput>({
+    resolver: zodResolver(timeIntervalsFormSchema),
+    defaultValues: {
+      intervals: [
+        { weekDay: 0, checked: false, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 1, checked: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 2, checked: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 3, checked: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 4, checked: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 5, checked: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 6, checked: false, startTime: '08:00', endTime: '18:00' },
+      ],
+    },
+  })
+  const weekDays = getWeekDays()
+
+  const { fields } = useFieldArray({
+    name: 'intervals',
+    control,
+  })
+
+  const intervals = watch('intervals')
+
+  function handleSetInterval(data: TimeIntervalsFormOutput) {
+    console.log(data)
+    console.log(errors)
+  }
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Quase lá!</Heading>
+        <Text>
+          Defina o intervalo de horários que você está disponivel em cada dia da
+          semana.
+        </Text>
+        <MultiStep size={4} currentStep={3} />
+      </Header>
+      {/* @ts-ignore */}
+      <IntervalBox as="form" onSubmit={handleSubmit(handleSetInterval)}>
+        <IntervalsContainer>
+          {fields?.map((item, index) => {
+            return (
+              <IntervalItem key={item.id}>
+                <IntervalDay>
+                  <Controller
+                    name={`intervals.${index}.checked`}
+                    control={control}
+                    render={({ field }) => {
+                      return (
+                        <Checkbox
+                          onCheckedChange={(checked) =>
+                            field.onChange(checked === true)
+                          }
+                          checked={field.value}
+                        />
+                      )
+                    }}
+                  />
+                  <Text>{weekDays[item.weekDay]}</Text>
+                </IntervalDay>
+                <IntervalInputs>
+                  <TextInput
+                    size="sm"
+                    type="time"
+                    step={60}
+                    {...register(`intervals.${index}.startTime`)}
+                    disabled={intervals[index].checked === false}
+                  />
+                  <TextInput
+                    size="sm"
+                    type="time"
+                    step={60}
+                    {...register(`intervals.${index}.endTime`)}
+                    disabled={intervals[index].checked === false}
+                  />
+                </IntervalInputs>
+              </IntervalItem>
+            )
+          })}
+        </IntervalsContainer>
+        {errors.intervals && <FormError>{errors.intervals.message}</FormError>}
+        <Button type="submit" disabled={isSubmitting}>
+          Próximo passo <ArrowRight />
+        </Button>
+      </IntervalBox>
+    </Container>
+  )
+}
