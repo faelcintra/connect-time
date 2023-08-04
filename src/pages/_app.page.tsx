fa68c01c756da97168bb8db69bc854588eff17bc
faelcintra <rafaacintramartins@gmail.com>
2023-08-03 23:50:11 -0300

    perf: :zap: Optimize availability api call with react-query

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,6 +3,8 @@ import '../lib/dayjs'
 import type { AppProps } from 'next/app'
 import { globalDefault } from '../styles/global'
 import { SessionProvider } from 'next-auth/react'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '../lib/react-query'
 
 globalDefault()
 
@@ -13,8 +15,10 @@ export default function App({
   // pageProps  étudo que retorna dentro de getServerSideProps,
   // mas por padrao session é undefined, ate que cria uma function getServerSideProps
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -6,10 +6,11 @@ import {
   TimePickerItem,
   TimePickerList,
 } from './styles'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import dayjs from 'dayjs'
 import { api } from '@/src/lib/axios'
 import { useRouter } from 'next/router'
+import { useQuery } from '@tanstack/react-query'
 
 interface Availability {
   possibleTimes: number[] // todos os horarios possiveis
@@ -18,7 +19,7 @@ interface Availability {
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
-  const [availability, setAvailability] = useState<Availability | null>(null)
+  // const [availability, setAvailability] = useState<Availability | null>(null)
 
   const router = useRouter()
   const username = String(router.query.username)
@@ -30,21 +31,42 @@ export function CalendarStep() {
     ? dayjs(selectedDate).format('DD [ de ] MMMM')
     : null
 
-  useEffect(() => {
-    if (!selectedDate) {
-      return
-    }
+  const selectedDateWithoutTime = selectedDate
+    ? dayjs(selectedDate).format('YYYY-MM-DD')
+    : null
 
-    api
-      .get(`/users/${username}/availability`, {
+  const { data: availability } = useQuery<Availability>(
+    ['availability', selectedDateWithoutTime], // segundo parametro é usado com uma dependencia para a query, se mudar, realizará uma nova busca na api para atualizar os dados
+    async () => {
+      const response = await api.get(`/users/${username}/availability`, {
         params: {
-          date: dayjs(selectedDate).format('YYYY-MM-DD'),
+          date: selectedDateWithoutTime,
         },
       })
-      .then((response) => {
-        setAvailability(response.data)
-      })
-  }, [selectedDate, username])
+
+      return response.data
+    },
+    {
+      enabled: !!selectedDate, // consulta só deve ser feita se for true, mesmo que selectedDateWithoutTime mude, se for selectedDate for false, nao buscara novamente
+    },
+  )
+
+  // useEffect(() => {
+  //   if (!selectedDate) {
+  //     return
+  //   }
+
+  //   api
+  //     .get(`/users/${username}/availability`, {
+  //       params: {
+  //         date: dayjs(selectedDate).format('YYYY-MM-DD'),
+  //       },
+  //     })
+  //     .then((response) => {
+  //       setAvailability(response.data)
+  //     })
+  // }, [selectedDate, username])
+
   return (
     <Container isTimePickerOpen={isDateSelected}>
       <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
